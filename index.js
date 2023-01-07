7dbf203252b8ecf7224b0e0b1a1ce320a148fb67
DeanLongmire <deanlongmire100@gmail.com>
2023-01-06 19:37:39 -0500

    Added variables
    added variables to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let name = "Dean";
+console.log(name);
+
+//cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number 
+//cannot containt ' ' or '-'
+//I like to use camel case for variable names
+//are case sensitive 
+//declare multiple variables by seperating by a comma or each one on a single line (best practice)
+
+const interestRate = 0.3;
+console.log(interestRate);
+
