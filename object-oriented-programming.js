a713662f89c1e02ace8815da087bb46d672f322d
DeanLongmire <deanlongmire100@gmail.com>
2023-01-07 14:36:20 -0500

    Update oop.js and Changed index.js
    added starter notes to oop.js and renamed index.js to intro as it was more appropriate

--- object-oriented-programming.js
+++ object-oriented-programming.js
@@ -13,3 +13,41 @@ let employee = {
 console.log(employee.getWage());
 
     //baseSalary, overtime, rate, and the function getWage are all apart of an employee object instead of being independent variables
+
+//Abstraction
+    //*think simple interface*
+    //reduces the impact of change
+
+//Inheritance
+    //eliminates reduntant code
+
+//Polymorphism
+    //Get rid of long if else or switch statements
+    //having a 'render' function for different objects inherited from an object that does different things dependednt on what the object requires of it
+    
+//Object Literals
+
+/*
+const circle = {
+    radius: 1,   //properties
+    location: {  //properties
+        x: 1,
+        y: 1
+    },
+    draw: function() //method
+    {  
+        console.log("draw");
+    }
+};
+
+circle.draw();
+*/
+
+//Factories (constructor function)
+    //say you want to make another circle, you would have to copy and paste above code
+    //Like classes and structs in C++
+
+function createCircle()
+{
+
+}
